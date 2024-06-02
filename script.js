4670b77055236ee3b599f18c638814ea470ad790
slowpoke <slowpoke901025@gmail.com>
2024-06-02 14:14:20 +0800

    10000 data

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ d3.csv("./dataset/car_prices_cleaned.csv").then(async (data) => {
   //   }
   //   return d;
   // });
-  data = data.slice(10000, 12000); // 10000, 16000
+  data = data.slice(10000, 20000); // 10000, 16000
   data.columns = columns;
   let __data = data;
   let _data = data;
